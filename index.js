1a4a7c2870f9ec5e3afbdaeaa1915e77c41d1f0f
Henry Miller <mail.henrym+git@gmail.com>
2022-01-02 10:48:50 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+//1
+const main = document.getElementById('main');
+main.remove();
+
+//2
+const newHeader = document.createElement("h1");
+newHeader.id="victory";
+const node = document.createTextNode("YOUR-NAME is the champion");
+newHeader.appendChild(node);
+document.body.append(newHeader);
